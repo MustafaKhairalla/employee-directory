77059799b99f01c28202bc67a0885236dfaf7fe1
mustafa khairalla <mukhairalla@gmail.com>
2020-05-07 16:07:30 -0600

    Updates

---
+++ precache-manifest.b1ef939690b7a0e5229cf28e84936dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753c74ed7da616780a5bd6be8a996dcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ba48fd2c4293e5733ec",
+    "url": "/static/css/main.b5af8621.chunk.css"
+  },
+  {
+    "revision": "97fc48e168da9ea2e33e",
+    "url": "/static/js/2.b262485f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b262485f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba48fd2c4293e5733ec",
+    "url": "/static/js/main.6be35fb7.chunk.js"
+  },
+  {
+    "revision": "a868036406e8eeaa172a",
+    "url": "/static/js/runtime-main.b9ed4eb0.js"
+  }
+]);
